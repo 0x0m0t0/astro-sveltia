f6692b70ab9394cc154d5aeac4dc870eb25defae
noodles4u <louisdesmett@gmail.com>
2023-11-03 21:58:01 +0100

    dark theme + nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  load: {
-    // Create a function to get the current build time
-    getBuildTime: () => {
-      return new Date().toISOString();
-    },
-  },
 });
