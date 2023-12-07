58d2454c6a1abfc0e46473f16f83a8f4be380975
noodles4u <louisdesmett@gmail.com>
2023-12-07 20:58:12 +0100

    update to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
-import node from '@astrojs/node'
 
 export default defineConfig({
 	output: 'hybrid',
 	adapter: vercel(),
-	adapter: node({
-		mode: 'standalone'
-	}),
+
 	integrations: [tailwind()]
 })
