0b8868d80ffb4072ac7495b3ae6f93f7aa253be6
noodles4u <louisdesmett@gmail.com>
2024-03-06 17:22:41 +0100

    author schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -22,6 +22,14 @@ const postsCollection = defineCollection({
 	})
 })
 
+const authorsCollection = defineCollection({
+	type: 'content',
+	schema: z.object({
+		name: z.string(),
+		image: z.string()
+	})
+})
+
 const aboutCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -33,35 +41,6 @@ const aboutCollection = defineCollection({
 
 export const collections = {
 	posts: postsCollection,
-	about: aboutCollection
+	about: aboutCollection,
+	authors: authorsCollection
 }
-
-// fields:
-// - { label: 'Title', name: 'title', widget: 'string' }
-// - { label: 'Publish Date', name: 'date', widget: 'datetime' }
-// - {
-//     label: 'Author',
-//     name: 'author',
-//     widget: 'relation',
-//     collection: 'authors',
-//     search_fields: 'name',
-//     value_field: 'name'
-//   }
-// - { label: 'Summary', name: 'summary', widget: 'text' }
-// - {
-//     label: 'Tags',
-//     name: 'tags',
-//     widget: 'list',
-//     default: ['post'],
-//     hint: 'Enter tags separated by a comma'
-//   }
-// - label: 'Image'
-//   name: 'image'
-//   widget: 'image'
-//   hint: 'Upload an image for this post. You can also use an image URL.'
-//   # Uncomment the lines below if you want to set a max file upload size e.g. 2MB
-//   #media_library:
-//   #config:
-//   #max_file_size: 2048000 # maximum image file size in bytes
-// - { label: 'Image Alt Text', name: 'imageAltText', widget: 'string' }
-// - { label: 'Body', name: 'body', widget: 'markdown' }
