7c7052fd9a7b102273caec34cffd1ab259ea6f4e
noodles4u <louisdesmett@gmail.com>
2024-05-28 23:14:24 +0200

    switch from vercel to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import cloudflare from '@astrojs/cloudflare'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-	output: 'hybrid',
+	output: 'server',
 	adapter: cloudflare({
 		imageService: 'cloudflare'
 	}),
